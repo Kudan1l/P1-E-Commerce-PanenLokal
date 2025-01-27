9344738650858b1ad7ed5fef0c8e11cc00c8f8f5
Kudan1l <alpsosial@gmail.com>
2025-01-27 16:21:25 +0700

    Build Vercel

--- resources/views/layouts/main-page.blade.php
+++ resources/views/layouts/main-page.blade.php
@@ -7,7 +7,7 @@
     <title>@yield('page-title', 'Panen Lokal - Hidroponik Sayuran')</title>
 
     {{-- Tailwind --}}
-    @vite('resources/css/app.css')
+    @vite('public/css/app.css')
     {{-- Bootstrap Icons --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     {{-- Alpine.js --}}
